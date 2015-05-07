c9cd090fd312d25a3cf829f3bac8dd41bc781284
He Gao <happykoekjes@gmail.com>
2015-05-06 23:27:17 -0500

    Add permissions to mybooks and checkout pages.

--- LibrarySystem/Site.Master.cs
+++ LibrarySystem/Site.Master.cs
@@ -10,12 +10,23 @@ namespace LibrarySystem
 {
     public partial class SiteMaster : MasterPage
     {
+        protected System.Web.UI.HtmlControls.HtmlGenericControl CheckoutLink;
+        protected System.Web.UI.HtmlControls.HtmlGenericControl MyBooksLink;
+ 
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Context.User != null && Context.User.Identity.IsAuthenticated)
             {
+                CheckoutLink.Visible = true;
+                MyBooksLink.Visible = true;
+
                 login.NavigateUrl = "~/Signout.aspx";
                 login.Text = "Signout";
+            }
+            else
+            {
+                CheckoutLink.Visible = false;
+                MyBooksLink.Visible = false;
             }
         }
     }
